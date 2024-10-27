dd33182ee6c2cf0add32f3958edf25555fc77827
Diego <diannewdev@gmail.com>
2024-10-27 13:33:57 -0400

    menu hamburguer adicionado, responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,46 @@ import ScrollMarker from "./componentes/ScrollMarker";
 import Repertorio from "./componentes/Repertorio";
 import Valores from "./componentes/Valores";
 import Contato from "./componentes/Contato";
+import TopBarHamburguer from "./componentes/TopBarHamburguer";
 
 
 const App: React.FC = () => {
 
+    //o seguinte código cuida de selecionar entre menu desktop
+    //e hamburguer, dependendo do tamanho da tela
+
+    const [abriuNoMobile, setAbriuNoMobile] = useState<boolean>(false);
+    const [larguraDaTela, setLarguraDaTela] = useState<number>(document.documentElement.clientWidth)
+
+    useEffect(() => {
+        function definirFormatodoHeader() {
+            if (document.documentElement.clientWidth < 639) {
+                setAbriuNoMobile(true);
+            } else {
+                setAbriuNoMobile(false)
+            }
+            
+            setLarguraDaTela(document.documentElement.clientWidth)
+        }
+
+
+
+        //chama a função na primeira renderização, equivale a um "onload"
+        definirFormatodoHeader();
+
+        //chama a função se o viewport tiver o tamanho alterado
+        window.addEventListener('resize', definirFormatodoHeader);
+
+        //limpa o listener (não sei pq é necessáriom mas é)
+        return () => {
+            window.removeEventListener('resize', definirFormatodoHeader)
+        }
+
+
+        
+    }, []); 
+
+    //se menu desktop, variavel para
     //controlar destaque do marcador de scroll
     const [scrollIndex, setScrollIndex] = useState<number>(1)
 
@@ -17,7 +53,10 @@ const App: React.FC = () => {
     return (
         <div className="max-w-[1400px] max-h-[1440px] flex justify-center items-center  static">
 
-            <Topbar setScrollIndex={setScrollIndex} />
+            {!abriuNoMobile && <Topbar setScrollIndex={setScrollIndex} />}
+            
+            {abriuNoMobile && <TopBarHamburguer larguraDaTela={larguraDaTela} />}
+
             
             <div id="scroller-horizontal" className="sm:no-scrollbar sm:escala sm:snap-x sm:snap-mandatory sm:snap-always overflow-x-hidden sm:overflow-x-auto sm:overflow-y-auto flex flex-col sm:flex-row items-center gap-16 sm:gap-32 w-full max-h-[1440px]">
                 <div id="hero" className="snap-center flex items-center justify-center p-0 xl:p-24 2xl:py-[10%] min-w-full h-full lg:h-screen max-h-[1440px]">
---
+++ src/componentes/TopBarHamburguer.tsx
@@ -0,0 +1,82 @@
+import React, {useEffect, useState} from "react";
+
+interface HeaderMobileProps {
+    larguraDaTela: number;
+}
+
+const TopBarHamburguer: React.FC<HeaderMobileProps> = ({larguraDaTela}) => {
+
+    //o seguinte código cuida de usar a prop para dinamicamente
+    //definir a posição left do painel hamburguer escondido
+    //à esquerda da margem esquerda
+    
+    const [hamburguerAberto, setHamburguerAberto] = useState(false);
+
+    function abrirMenu() {
+        if (hamburguerAberto) {
+            setHamburguerAberto(false)
+        } else {
+            setHamburguerAberto(true)
+        }
+
+    }
+
+    const posicaoEsquerda = hamburguerAberto ? 0 : -larguraDaTela;
+
+    //o seguinte código cuida de detectar que o usuário rolou a página
+    //para baixo para aplicar leve sombreamento no header
+
+    const [rolouScroll, setRolouScroll] = useState(false);
+
+    useEffect(() => {
+        console.log('useEffect chamada: sim')
+        const seRolarScroll = () => {
+            console.log('função chamada: não')
+            if (window.scrollY > 50) {
+                console.log('if chamado: não')
+                setRolouScroll(true);
+            } else {
+                setRolouScroll(false)
+                console.log('else chamado: não');
+            }
+        }
+
+
+        window.addEventListener('scroll', seRolarScroll);
+
+        return () => {
+            window.removeEventListener('scroll', seRolarScroll);
+        };
+
+    }, []);
+    
+
+    return (
+        <div className="relative">
+            <div className={`z-50 w-full fixed top-0 left-0 px-8 py-5 b`}>
+                <button onClick={abrirMenu} className='z-10 flex flex-col gap-2'>
+                    <div className='z-50 w-8 h-0.5 bg-sky-200'></div>
+                    <div className='z-50 w-8 h-0.5 bg-sky-200'></div>
+                    <div className='z-50 w-8 h-0.5 bg-sky-200'></div>
+                </button>
+
+            <img className='z-40 px-8 py-5 absolute top-0 right-0 w-[100px] scale-[1]' src={"./assets/images/logo.png"} alt="Logo da empresa" />
+
+                <div 
+                className={`z-40 absolute w-full h-screen top-0 left-0 flex flex-col gap-5 p-8 pt-20 bg-gray-800/100 font-bold hamburguer-transition`}
+                style={{left: `${posicaoEsquerda}px` }}
+                >
+                    <a onClick={abrirMenu} className='z-40 text-xl font-normal text-white' href="#hero">Início</a>
+                    <a onClick={abrirMenu} className='z-40 text-xl font-normal text-white' href="#repertorio">Portfolio</a>
+                    <a onClick={abrirMenu} className='z-40 text-xl font-normal text-white' href="#valores">Valores</a>
+                    <a onClick={abrirMenu} className='z-40 text-xl font-normal text-white' href="#contato">Contato</a>
+                </div>
+
+            </div> 
+
+            <div id='problem' className={`z-40 w-full fixed top-0 left-0 p-8 transicao-gradiente ${rolouScroll ? 'gradiente' : 'gradiente'}`}></div>
+        </div>
+    );
+}
+
+export default TopBarHamburguer;
